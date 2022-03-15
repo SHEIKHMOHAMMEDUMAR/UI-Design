15bddd2511abe09ca0a5067760f9bd25fc34e9de
SHEIKH MOHAMMED UMAR <mohdumarfarooq123@gmail.com>
2022-03-15 12:42:24 +0530

    Hamburger Icon

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -9,8 +9,8 @@ import {
 export default function Chart({title, data, dataKey, grid}) {
   return (
     <div className="chart">
-        <h3 className="chartTitle">{title}</h3>
-        <ResponsiveContainer width="100%" aspect={4/1}>
+        <h2 className="chartTitle">{title}</h2>
+        <ResponsiveContainer width="90%" aspect={4/1}>
             <LineChart data={data}>
                 <XAxis dataKey="name" stroke="#5550bd"/>
                 <Line type="monotone" dataKey={dataKey} stroke="#5550bd"/>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import "./topbar.css"
 import { Notifications, Language, Settings } from '@material-ui/icons'
+import { Menu } from '@material-ui/icons';
 const { faker } = require('@faker-js/faker');
 export default function topbar() {
   return (
     <div className='topbar'>
       <div className='topbarWrapper'>
-        <div className='topleftb'>
-          <span className='logo'> Perfios Software Solution Ltd. Std.</span>  
-        </div> 
         <div className='toplefta'>
-          <span className='logo'> PSSLS</span> 
-        </div>  
+          <span className='logo'> Perfios Software Solution Ltd. Std.</span>
+        </div>
+        <div className='topleftb'>
+          <Menu className='menuIcon'/>
+          <span className='logo'> PSSLS</span>  
+        </div>   
         <div className='topright'>
           <div className="topbarIconsContainer"> 
             <Notifications />
             <span className="topbarIconsBadges">2</span>
           </div>
-          <div className="topbarIconsContainer"> 
-            <Language />
-            <span className="topbarIconsBadges">2</span>
+          <div className="lan">
+            <div className="topbarIconsContainer"> 
+              <Language />
+              <span className="topbarIconsBadges">2</span>
+            </div>
           </div>
-          <div className="topbarIconsContainer"> 
-            <Settings />  
+          <div className="set">
+            <div className="topbarIconsContainer"> 
+              <Settings/>  
+            </div>
           </div>
           <div className="topbarIconsContainer"> 
             <img src= {faker.image.avatar()} alt="" className='topbaravatar'/>  
