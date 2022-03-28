b027612a79419f57f9fe0bb1395f30c3849699b8
SHEIKH MOHAMMED UMAR <mohdumarfarooq123@gmail.com>
2022-03-28 16:18:59 +0530

    updat

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,16 +6,16 @@ import WidgetSmall from "../../components/widgetSmall/WidgetSmall"
 import Widgetlarge from "../../components/widgetLarge/WidgetLarge"
 const { faker } = require('@faker-js/faker');
 const Litems = [
-    { img: faker.image.avatar(), name: "Susan Carol", date: "2 Jan 2021", Amount: "$122.00", Button: "Approved"},
-    { img: faker.image.avatar(), name: "Umar Carol", date: "2 Jan 2021", Amount: "$122.00", Button: "Approved"},
-    { img: faker.image.avatar(), name: "Susan Carol", date: "2 Jan 2021", Amount: "$122.00", Button: "Declined"},
-    { img: faker.image.avatar(), name: "Susan Carol", date: "2 Jan 2021", Amount: "$122.00", Button: "Declined"},
+    { img: faker.image.avatar(), name: "Susan Carol", date: "02 Jan 2020", Amount: "$122.00", Button: "Approved"},
+    { img: faker.image.avatar(), name: "Jason Mathew", date: "12 Jun 2020", Amount: "$1200.00", Button: "Approved"},
+    { img: faker.image.avatar(), name: "Jasmin Carol", date: "22 Sep 2021", Amount: "$2211.00", Button: "Declined"},
+    { img: faker.image.avatar(), name: "Johnny Deep", date: "30 Mar 2022", Amount: "$2022.00", Button: "Declined"},
 ]
 const Sitems = [
   { img: faker.image.avatar(), name: "Susan Carol", title: "Software Engineer."},
-  { img: faker.image.avatar(), name: "Umar Carol", title: "Software Engineer."},
-  { img: faker.image.avatar(), name: "Susan Carol", title: "Software Engineer."},
-  { img: faker.image.avatar(), name: "Susan Carol", title: "Software Engineer."}
+  { img: faker.image.avatar(), name: "Jason Mathew", title: "Network Engineer."},
+  { img: faker.image.avatar(), name: "Jasmin Carol", title: "Doctor"},
+  { img: faker.image.avatar(), name: "Johnny Deep", title: "Architect"}
 ]
 const Feitems = [
   { title: "Revenue", cost: "$2,415", rate: +11.4},
